bcdd0a1ea2ed5eceb82cbc8a4a7c349239c4e5f9
tadesros <tomadesrosiers@gmail.com>
2021-11-17 21:39:06 -0500

    Express Session Part 2

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+//Export the model from this module 
+//Model has name User-> collection named users and the schema
+module.exports = mongoose.model('User', userSchema);
